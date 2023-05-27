1ac516cf4cd99950d1fba2af530a4b6e8520031c
NickSchlederer <nschled4665@gmail.com>
2023-05-27 21:21:38 +1000

    Finalised the update password function
    still need to resolve the double segue

--- FlashyQuiz/MainMenuNav/Controllers/SettingsPageVC.swift
+++ FlashyQuiz/MainMenuNav/Controllers/SettingsPageVC.swift
@@ -10,13 +10,57 @@ import UIKit
 
 class SettingsPageVC: UIViewController {
     
-    var LoggedUser: User?
     
+    @IBOutlet weak var Error_Label: UILabel!
+    @IBOutlet weak var CurrentPassWordTF: UITextField!
+    
+    @IBOutlet weak var PasswordOneTF: UITextField!
+    
+    @IBOutlet weak var PasswordTwoTF: UITextField!
+    
+    @IBOutlet weak var UpdateButton: UIButton!
+    
+    var LoggedUser: User?
+    let userManager = UserManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let TabCont = self.tabBarController as! BaseTabBarController
         LoggedUser = TabCont.loggedUser;
+        Error_Label.isHidden = true;
+    }
+    
+    
+    @IBAction func UpdateButtonPressed(_ sender: Any) {
+        
+        let currentEmail = LoggedUser?.email ?? ""
+        let currentPassword = CurrentPassWordTF.text ?? ""
+        
+        let newPasswordOne = PasswordOneTF.text ?? ""
+        let newPasswordTwo = PasswordTwoTF.text ?? ""
+        
+        if userManager.loginUser(email: currentEmail, password: currentPassword) != nil { // checks if the current password would log into the current account
+            
+            if (newPasswordOne == newPasswordTwo) {
+                LoggedUser?.setPassword(oldPassword: currentPassword, newPassword: newPasswordOne)
+                Error_Label.isHidden = false
+                Error_Label.text = "Password Updated!"
+                Error_Label.textColor = .green
+                
+                
+            } else {
+                Error_Label.textColor = .red
+                Error_Label.isHidden = false
+                Error_Label.text = "New Passwords Dont Match"
+            }
+            
+        } else {
+            Error_Label.textColor = .red
+            Error_Label.isHidden = false
+            Error_Label.text = "Incorrect Current Password"
+        }
+        
+        
     }
     
 
