a24ecc4c53e626035db2109ef23f7685e8f52bea
Eren <eren_atilgan@hotmail.com>
2023-05-27 00:24:08 -0600

    Updated getUserQuizzes to address error

--- FlashyQuiz/Database/Model/QuizManager.swift
+++ FlashyQuiz/Database/Model/QuizManager.swift
@@ -2,20 +2,19 @@ import Foundation
 import SQLite
 
 struct QuizManager {
-    
+
     let databaseURL = DatabaseManager().getDatabasePath()
-    
-    // Function to retrieve user details by ID
-    // Returns ID - Username - Email
+
     func getUserQuizzes(userId: Int) -> [Quiz]? {
         var quizzes: [Quiz] = []
         var questions: [Question] = []
+
         do {
             let db = try Connection(databaseURL)
-            
-            let quizzesView = View("FullQuiz") // Changed from Table() to View(), if encountering an error, switch back
+
+            let quizzesView = View("FullQuiz")
             let quizId = Expression<Int>("id")
-            var prevQuizId : Int = 0// To be used as tracking between quizzes
+            var prevQuizId: Int = 0
             let userId = Expression<Int>("userId")
             let title = Expression<String>("title")
             let privacy = Expression<String>("privacy")
@@ -24,37 +23,39 @@ struct QuizManager {
             let incorrectAnswer1 = Expression<String>("incorrectAnswer1")
             let incorrectAnswer2 = Expression<String>("incorrectAnswer2")
             let incorrectAnswer3 = Expression<String>("incorrectAnswer3")
-            
-            let query = quizzesView.filter(quizzesView[userId] == userId) // Filter view to quizzes owned by provided userId
-            let rows = try db.prepare(query) // Query view
-            let totalRowCount = rows.lazy.map { _ in 1 }.reduce(0, +) // get total row count of data fetched
-            
-            for (index, row) in rows.enumerated() { // Iterate through rows of data, assigning values according to row data
+
+            let query = quizzesView.filter(quizzesView[userId] == userId)
+            let rows = try db.prepare(query)
+            let totalRowCount = rows.lazy.map { _ in 1 }.reduce(0, +)
+            var index = 0
+
+            for row in rows {
                 let quizIdValue = row[quizId]
                 let userIdValue = row[userId]
                 let titleValue = row[title]
                 let privacyValue = row[privacy]
                 let questionValue = row[question]
                 let correctAnswerValue = row[correctAnswer]
-                let incorrectAnswers = [row[incorrectAnswer1], row[incorrectAnswer2], row[incorrectAnswer3]].compactMap { $0 } // Collect incorrect answers
-                let question = Question(question: questionValue, correctAnswer: correctAnswerValue, incorrectAnswers: incorrectAnswers) // Create Question
-                
-                if prevQuizId == 0 { // First row of data
-                    questions.append(question) // Append created question to array/list
-                } else if quizIdValue == prevQuizId { // Same quiz as prior row
-                    questions.append(question) // Append created question to array/list
-                } else { // Not the same quiz as prior row
-                    let quiz = Quiz(quizId: quizIdValue, userId: userIdValue, title: titleValue, privacy: privacyValue, questions: questions) // Create Quiz
-                    quizzes.append(quiz) // Append quiz to array/list
-                    questions = [] // Reset questions array/list
-                    questions.append(question) // Append created question to array/list
+                let incorrectAnswers = [row[incorrectAnswer1], row[incorrectAnswer2], row[incorrectAnswer3]].compactMap { $0 }
+                let question = Question(question: questionValue, correctAnswer: correctAnswerValue, incorrectAnswers: incorrectAnswers)
+
+                if prevQuizId == 0 {
+                    questions.append(question)
+                } else if quizIdValue == prevQuizId {
+                    questions.append(question)
+                } else {
+                    let quiz = Quiz(quizId: prevQuizId, userId: userIdValue, title: titleValue, privacy: privacyValue, questions: questions)
+                    quizzes.append(quiz)
+                    questions = []
+                    questions.append(question)
                 }
-                prevQuizId = quizIdValue // reassign prevQuizId before next iteration
-                
-                if index == totalRowCount - 1 { // Check if it's the last row of data, if it is, create and append final quiz
-                    let quiz = Quiz(quizId: quizIdValue, userId: userIdValue, title: titleValue, privacy: privacyValue, questions: questions) // Create Quiz
-                    quizzes.append(quiz) // Append quiz to array/list
+                prevQuizId = quizIdValue
+
+                if index == totalRowCount - 1 {
+                    let quiz = Quiz(quizId: quizIdValue, userId: userIdValue, title: titleValue, privacy: privacyValue, questions: questions)
+                    quizzes.append(quiz)
                 }
+                index += 1
             }
         } catch {
             print("Error retrieving quizzes: \(error)")
@@ -62,3 +63,4 @@ struct QuizManager {
         return quizzes
     }
 }
+
