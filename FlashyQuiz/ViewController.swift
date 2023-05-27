798be4856919fac68b1a4c1d1fd6a133b04fa653
Eren <eren_atilgan@hotmail.com>
2023-05-26 23:52:54 -0600

    Added test for getUserQuizzes

--- FlashyQuiz/ViewController.swift
+++ FlashyQuiz/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         let userManager = UserManager()
+        let quizManager = QuizManager()
         
         if let userDetails = userManager.getUserDetails(userId: 1) {
             print("User ID: \(userDetails.id)")
@@ -18,6 +19,22 @@ class ViewController: UIViewController {
             print("Email: \(userDetails.email)")
         } else {
             print("User not found.")
+        }
+        
+        if let quizzes = quizManager.getUserQuizzes(userId: 1) {
+            for quiz in quizzes {
+                print("Quiz ID: \(quiz.quizId ?? -1)")
+                print("Quiz Title: \(quiz.title)")
+                print("Quiz Privacy: \(quiz.privacy)")
+                print("Quiz UserId: \(quiz.userId)")
+                for question in quiz.questions {
+                    print("Question: \(question.question)")
+                    print("Question Answer: \(question.correctAnswer)")
+                    print("Question Incorrect Answers: \(question.incorrectAnswers)")
+                }
+            }
+        } else {
+            print("Error")
         }
 
         super.viewDidLoad()
