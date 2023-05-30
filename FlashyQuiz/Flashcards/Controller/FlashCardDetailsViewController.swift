7ac2e794c109448ecc91ad6ed02b382d66f3e80a
jeremychye <jeremychye@gmail.com>
2023-05-30 16:00:28 +1000

    we good

--- FlashyQuiz/Flashcards/Controller/FlashCardDetailsViewController.swift
+++ FlashyQuiz/Flashcards/Controller/FlashCardDetailsViewController.swift
@@ -13,8 +13,8 @@ class FlashCardDetailsViewController: UIViewController, UITableViewDelegate, UIT
     @IBOutlet weak var flashcardTable: UITableView!
     
     var flashcardGroup: FlashcardGroup?
-    var questions: [String] = [] // Placeholder for questions
-    var answers: [String] = [] // Placeholder for answers
+    var flashcardManager = FlashcardManager()
+    var isComingFromExplorePage: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +23,13 @@ class FlashCardDetailsViewController: UIViewController, UITableViewDelegate, UIT
         
         let nib = UINib(nibName: "FlashCardCell", bundle: nil)
         flashcardTable.register(nib, forCellReuseIdentifier: "FlashCardCell")
-        
         flashcardTable.delegate = self
         flashcardTable.dataSource = self
+        
+        if isComingFromExplorePage {
+            // Hide the delete button if coming from the explore page
+            navigationItem.rightBarButtonItem = nil
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,22 +39,72 @@ class FlashCardDetailsViewController: UIViewController, UITableViewDelegate, UIT
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FlashCardCell", for: indexPath) as! FlashCardCell
         
-        let flashcard = flashcardGroup?.flashcards[indexPath.row]
-        cell.flashCardTitle.text = flashcard?.question
-        cell.termLabel.text = flashcard?.answer
+        guard let flashcard = flashcardGroup?.flashcards[indexPath.row] else {
+            return cell
+        }
+        
+        cell.flashCardTitle.text = flashcard.question
+        cell.termLabel.text = flashcard.answer
         
         // Configure other cell properties based on the flashcard
         
         return cell
     }
     
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+        // Create an alert to confirm deletion
+        let alert = UIAlertController(title: "Delete Flashcard Group",
+                                      message: "Are you sure you want to delete this flashcard group?",
+                                      preferredStyle: .alert)
+
+        // Add a cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+
+        // Add a delete action
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            guard let self = self else {
+                return
+            }
+
+            if let flashcardGroup = self.flashcardGroup {
+                let isDeleted = self.flashcardManager.deleteFlashcardGroup(flashcardGroup: flashcardGroup)
+
+                if isDeleted {
+                    self.displayAlertWithCompletion(message: "Flashcard group deleted successfully") {
+                        self.navigateBackToPage()
+                    }
+                } else {
+                    self.displayAlert(message: "Failed to delete flashcard group")
+                }
+            }
+        }
+        alert.addAction(deleteAction)
+
+        // Present the alert
+        present(alert, animated: true, completion: nil)
+    }
+
+    
+    func displayAlertWithCompletion(message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func displayAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             switch identifier {
-            case "goToModifyFlashcard":
-                if let modifyFlashcardVC = segue.destination as? EditFlashCardViewController {
-                
-                                    }
             case "goToPlayFlashcard":
                 if let playFlashcardVC = segue.destination as? ViewFlashcardsViewController {
                     playFlashcardVC.flashCardGroup = flashcardGroup
@@ -62,6 +116,10 @@ class FlashCardDetailsViewController: UIViewController, UITableViewDelegate, UIT
             }
         }
     }
-
+    
+    func navigateBackToPage() {
+        if let quizMainMenuVC = navigationController?.viewControllers.first(where: { $0 is BaseTabBarController }) {
+            navigationController?.popToViewController(quizMainMenuVC, animated: true)
+        }
+    }
 }
-
