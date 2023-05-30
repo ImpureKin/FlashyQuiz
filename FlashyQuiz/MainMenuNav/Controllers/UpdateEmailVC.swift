65066683a22e0753d5daa0459bb922d7b56f4c68
NickSchlederer <nschled4665@gmail.com>
2023-05-30 15:57:47 +1000

    Some errors still exist but updates story board

---
+++ FlashyQuiz/MainMenuNav/Controllers/UpdateEmailVC.swift
@@ -0,0 +1,51 @@
+//
+//  UpdateEmailVC.swift
+//  FlashyQuiz
+//
+//  Created by Nicholas  Schlederer on 30/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class UpdateEmailVC : UIViewController {
+    
+    var LoggedUser: User?
+    let userManager = UserManager()
+    
+    
+    @IBOutlet weak var CurrentEmailTF: UITextField!
+    
+    @IBOutlet weak var newEmailTF: UITextField!
+    
+    @IBOutlet weak var RepeatNewEmailTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func UpdateEmailButton(_ sender: Any) {
+        
+        let currentEmail = CurrentEmailTF.text ?? ""
+        let newEmailOne = newEmailTF.text ?? ""
+        let newEmailTwo = RepeatNewEmailTF.text ?? ""
+        
+        if (currentEmail == LoggedUser?.email) {
+            if (newEmailOne == newEmailTwo) {
+                LoggedUser?.setEmail(newEmail: newEmailOne)
+            } else {
+                let alert = UIAlertController(title: "Mismatched Emails",message: "The new Emails provided do not match. Please ensure they are identical.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+            }
+        } else {
+            let alert = UIAlertController(title: "Incorrect Current Email",message: "The current email provided does not match the email for this account.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+}
---
+++ FlashyQuiz/MainMenuNav/Controllers/UpdateUsernameVC.swift
@@ -0,0 +1,26 @@
+//
+//  UpdateUsernameVC.swift
+//  FlashyQuiz
+//
+//  Created by Nicholas  Schlederer on 30/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class UpdateUsernameVC : UIViewController {
+    
+    
+    var LoggedUser: User?
+    let userManager = UserManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+}
